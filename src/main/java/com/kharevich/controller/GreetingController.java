02b75f54d0d51c0de2d57af22a1c98dded9b3402
Khorevich Dmitry <dmitry.khorevich@altoros.com>
2015-06-12 19:12:12 +0300

    code not working

--- src/main/java/com/kharevich/controller/GreetingController.java
+++ src/main/java/com/kharevich/controller/GreetingController.java
@@ -2,6 +2,8 @@ package com.kharevich.controller;
 
 import com.kharevich.bean.Greeting;
 import com.kharevich.component.EnvironmentComponent;
+import com.kharevich.component.RabbitMQAppender;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +16,20 @@ public class GreetingController {
     @Autowired
     private EnvironmentComponent environmentComponent;
 
+    @Autowired
+    RabbitMQAppender logger1;
+
+
+    private static  final Logger logger = Logger.getLogger(GreetingController.class);
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     @ResponseBody
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        logger.error("greeting method");
+        logger.error(logger1.getUsername());
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, environmentComponent.getUserName()));
     }
